c2efbd9cca0fd8d3b8e2c46093f07c62ae26b3d9
plambert684 <plambert684@gmail.com>
2026-03-01 16:48:22 +0100

    Initial commit

---
+++ src/main/java/org/plambert/oneShot/OneShot.java
@@ -0,0 +1,16 @@
+package org.plambert.oneShot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OneShot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
